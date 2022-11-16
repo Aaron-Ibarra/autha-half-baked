73baf2a5f7c0569de09e713f5e7c567eb40bb362
Aaron Ibarra <aaronibarra132@gmail.com>
2022-11-16 13:20:22 -0800

    redirects to other page when user signs in

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
+import { getUser, signInUser, signupUser } from './fetch-utils.js';
+
 const signInForm = document.getElementById('sign-in');
 
 const signUpForm = document.getElementById('sign-up');
 
 // Wire up sign in and sign up forms to supabase
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    const email = data.get('email');
+    const password = data.get('password');
+    const user = await signupUser(email, password);
+    if (user) {
+        location.replace('/other-page');
+    }
+});
+
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signInForm);
+    const email = data.get('email');
+    const password = data.get('password');
+    const user = await signInUser(email, password);
+
+    if (user) {
+        location.replace('/other-page');
+    }
+});
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,7 @@
 // Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://gvhgebiiqjezrhkmcque.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2aGdlYmlpcWplenJoa21jcXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwMzcsImV4cCI6MTk4MzY4NDAzN30.IPXrWCU6kYe9JaHUfGPnQcuwKDHonUpaRuCgs3uCEok';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -8,9 +9,21 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
 
-export async function signInUser(email, password) {}
+    if (response.user) {
+        return response.user;
+    } else {
+        console.error(response.error);
+    }
+}
+
+export async function signInUser(email, password) {
+    const response = await client.auth.signIn({ email, password });
+
+    return response.user;
+}
 
 export async function checkAuth() {}
 
